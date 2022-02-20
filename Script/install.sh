fd4d007df304e6ef1571034ea9b0d4e897c490b6
Steve J. South(NamJungGu) <nowage@gmail.com>
2022-02-20 13:59:41 +0900

    add j2mAll , m2jAll

--- Script/install.sh
+++ Script/install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-j2mAll m2jAll
+currentPath=$(pwd)
+thePath=${BASH_SOURCE[0]}
+sourceFile=$(pwd)/$thePath
+sourcePath=`echo $(dirname $sourceFile)|sed "s/\/\.\//\//g"|sed "s/\/\.//g"`
+
+# echo sourcePath=$sourcePath
+# echo currentPath=$currentPath
+
+cd $sourcePath
 for i in p2j j2m m2p p2m m2j j2p md2ipynb j2mAll m2jAll; do
-    cp $i /usr/local/bin/
-    chmod /usr/local/bin/$i
+    echo copy $i to /usr/local/bin/$i
+    sudo cp $i /usr/local/bin/
+    sudo chmod +x /usr/local/bin/$i
 done
+
+cd $currentPath
